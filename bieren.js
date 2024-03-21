ea7db8025b93fcae3aeecf53174bde6d1c410c60
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2024-03-21 23:18:11 +0100

    (Slightly) improve beerDisplay loading times, improve bieren overview

--- bieren.js
+++ bieren.js
@@ -1,5 +1,24 @@
 "use strict";
 
+
+// select all needed elements
+const beerName = document.getElementById("beerName");
+const beerTagline = document.getElementById("beerTagLine");
+const beerDescription = document.getElementById("beerDescription");
+const beerInfo = document.getElementById("beerInfo");
+const beerImage = document.getElementById("bierImg");
+const beerLogo = document.getElementById("beerLogo");
+const colors = document.querySelectorAll(".themed");
+const buyButton = document.querySelector(".buy-button2");
+const neonborder = document.querySelector(".imgBorder");
+const root = document.querySelector(":root");
+const beerBackground = document.querySelector(".beerBackground");
+const navLogo = document.querySelector(".logo");
+const nav = document.querySelectorAll("nav ul li");
+const awards = document.querySelector(".awards");
+
+
+
 let beer = 0;
 let allBeer = [];
 const bierNames = [
@@ -22,19 +41,17 @@ function getParamBeer() {
   // zoek de biernaam op in de url -> ?beer=""
   // selecteer de array index op basis van de naam
   const bierParams = new URLSearchParams(window.location.search).get("beer");
-  console.log(bierParams);
 
   const bierIndex = bierNames.indexOf(bierParams);
+
   return bierIndex;
 }
 function fetchBeer() {
   fetch("data/beerData.json")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       // showData(data);
       allBeer = data;
-      console.warn(getParamBeer());
       renderBeer(data, getParamBeer());
     });
 }
@@ -43,34 +60,15 @@ function fetchBeer() {
 // );
 
 function renderBeer(data, beer) {
-  const beerName = document.getElementById("beerName");
-  const beerTagline = document.getElementById("beerTagLine");
-  const beerDescription = document.getElementById("beerDescription");
-  const beerInfo = document.getElementById("beerInfo");
-  const beerImage = document.getElementById("bierImg");
-  const beerLogo = document.getElementById("beerLogo");
-  const colors = document.querySelectorAll(".themed");
-  const buyButton = document.querySelector(".buy-button2");
-  const neonborder = document.querySelector(".imgBorder");
-  const root = document.querySelector(":root");
-  const beerBackground = document.querySelector(".beerBackground");
-  const navLogo = document.querySelector(".logo");
-  const nav = document.querySelectorAll("nav ul li");
-  const awards = document.querySelector(".awards");
-  console.error(nav);
   // Verander thema kleur (zie css variabelen in bierDisplay.css)
-  console.warn(data);
   root.style.setProperty("--themeColor", data[beer].themeColor);
   neonborder.style.filter = `drop-shadow(0 0 0.75rem ${data[beer].neonColor})`;
-  console.warn(data[beer].beerLogo);
 
   if (data[beer].awards) {
-    console.log("TRUE");
     awards.innerHTML = "";
     // awards.innerHTML = `<h2>-Awards-</h2>`;
     data[beer].awards.forEach((award) => {
       awards.innerHTML += `<li> <img src="${award}" alt="award"></li>`;
-      console.log(award);
     });
   } else{
     awards.innerHTML = "";
@@ -78,29 +76,21 @@ function renderBeer(data, beer) {
 
   // veranderd de styling van het logo wanner het het wilderen logo is
   if (data[beer].beerLogo == "WEBSITE/Logo/Wilderen Logo 2020.png") {
-    console.warn(data[beer].beerlogo);
     beerLogo.style.border = "none";
     beerLogo.style.borderRadius = "0";
     beerLogo.style.height = "50%";
     // beerLogo.style.transform = "translate(0%, -100%)";
   } else {
     beerLogo.style.border = `5px solid ${data[beer].themeColor}`;
-    // beerLogo.style.borderRadius = "0";
-    // beerLogo.style.objectFit = "cover";
-    // beerLogo.style.width = "40%";
-    // beerLogo.style.height = "50%";
-    // beerLogo.style.transform = "translate(0%, -130%)";
   }
   if (data[beer].beerLogo == "") {
     beerLogo.style.display = "none";
-    console.log(data[beer].beerLogo);
     beerLogo.style.display = "none";
   } else {
     beerLogo.style.display = "block";
   }
 
   if (data[beer].id == 5 || data[beer].id == 6) {
-    console.log(data[beer].id);
     beerName.style.fontSize = "1.5rem";
   } else {
     beerName.style.fontSize = "2rem";
@@ -114,11 +104,8 @@ function renderBeer(data, beer) {
   }
   colors.forEach((color) => {
     color.style.borderColor = data[beer].themeColor;
-    console.log(data[beer].themeColor);
-    console.log(colors);
   });
 
-  console.log(beerImage);
   beerName.innerHTML = data[beer].beerName;
   beerTagline.innerHTML = data[beer].beerTagline;
   beerDescription.innerHTML = data[beer].beerDescription;
@@ -127,12 +114,10 @@ function renderBeer(data, beer) {
 
   data[beer].beerInfo.forEach((beer) => {
     beerInfo.innerHTML += `<li>${beer}</li>`;
-    console.log(beer);
   });
 
   // beerImage.src = data[beer].beerImg;
   beerLogo.innerHTML = data[beer].logo;
-  console.warn(data[beer].id);
   if (blackTextNeeded.includes(data[beer].id)) {
     // beerName.style.color = "black";
     beerTagline.style.color = "black";
@@ -168,10 +153,6 @@ function renderBeer(data, beer) {
 
 function buttonClick() {
   leftArrow.addEventListener("click", function () {
-    console.log("!!!!!!!CLICKED!!!!!!!!!");
-    console.log(beer);
-    console.log(getParamBeer());
-    console.warn(getParamBeer());
     if (getParamBeer() > 0) {
       beer--;
       updateParams(getParamBeer() - 1);
@@ -179,26 +160,18 @@ function buttonClick() {
     }
   });
   rightArrow.addEventListener("click", function () {
-    console.log("!!!!!!!CLICKED!!!!!!!!!");
-    console.log(beer);
-    console.log(getParamBeer());
-    console.warn(allBeer.length - 1);
     if (getParamBeer() < allBeer.length - 1) {
       beer++;
       updateParams(getParamBeer() + 1);
       renderBeer(allBeer, getParamBeer());
 
-      console.warn(getParamBeer());
     }
   });
 }
 
 function updateParams(param) {
-  console.warn("getting updated");
   const bierIndex = bierNames[param];
-  console.log(bierIndex);
   history.pushState(null, null, `?beer=${bierIndex}`);
-  console.log(history);
 }
 
 fetchBeer();
