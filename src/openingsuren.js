fc9641b5223bc84bf342f4d34b8c303c09cac6a9
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2024-12-12 21:30:46 +0100

    feat: add new opening hours
    style: center opening hours and info container

--- src/openingsuren.js
+++ src/openingsuren.js
@@ -12,10 +12,16 @@ function renderTables(data) {
   const table1 = document.getElementById("table1");
   const table2 = document.getElementById("table2");
   const table3 = document.getElementById("table3");
+  const table4 = document.getElementById("table4");
+  const table5 = document.getElementById("table5");
+  const table6 = document.getElementById("table6");
 
   table1.innerHTML = "";
   table2.innerHTML = "";
   table3.innerHTML = "";
+  table4.innerHTML = "";
+  table5.innerHTML = "";
+  table6.innerHTML = "";
 
   //---April/September---
 
@@ -209,6 +215,158 @@ function renderTables(data) {
                     </td>
                     `;
 
+  table4.innerHTML = `
+                    <th data-i18n="tableDateSpan.0" colspan="2">${data.headers[0]}</th>
+            
+                    <tr>
+                    
+                    <td data-i18n="table.0.table1.row1.0" >${data.table1.row1[0]}</td>
+                    <td data-i18n="table.0.table1.row1.1"" >${data.table1.row1[1]}</td>
+                </tr>
+                <tr>
+                
+                    <td data-i18n="table.0.table1.row2.0">${data.table1.row2[0]}</td>
+                    <td data-i18n="table.0.table1.row2.1">${data.table1.row2[1]}</td>
+                </tr>
+                <tr>
+                
+            
+                    <td data-i18n="table.0.table1.row3.0">${data.table1.row3[0]}</td>
+                    <td data-i18n="table.0.table1.row3.1">${data.table1.row3[1]}</td>
+                </tr>
+                <tr>
+                
+            
+                    <td data-i18n="table.0.table1.row4.0">${data.table1.row4[0]}</td>
+                    <td data-i18n="table.0.table1.row4.1">${data.table1.row4[1]}</td>
+                </tr>
+                <tr>
+                
+            
+                    <td data-i18n="table.0.table1.row5.0">${data.table1.row5[0]}</td>
+                    <td data-i18n="table.0.table1.row5.1">${data.table1.row5[1]}</td>
+                </tr>
+                <tr>
+                
+            
+                    <td data-i18n="table.0.table1.row6.0">${data.table1.row6[0]}</td>
+                    <td data-i18n="table.0.table1.row6.1">${data.table1.row6[1]}</td>
+                </tr>
+                <tr>
+                
+            
+                    <td data-i18n="table.0.table1.row7.0">${data.table1.row7[0]}</td>
+                    <td data-i18n="table.0.table1.row7.1">${data.table1.row7[1]}</td>
+                </tr>
+                `;
+
+  //---Oktober/Maart---
+
+  table5.innerHTML = `
+                
+                        <th data-i18n="tableDateSpan.1" colspan="2">${data.headers[1]}</th>
+                        <tr>
+                        <td data-i18n="table.0.table2.row1.0">${data.table2.row1[0]}</td>
+                        <td data-i18n="table.0.table2.row1.1">${data.table2.row1[1]}</td>
+                    </tr>
+                    <tr>
+                        <td data-i18n="table.0.table2.row2.0">${data.table2.row2[0]}</td>
+                        <td data-i18n="table.0.table2.row2.1">${data.table2.row2[1]}</td>
+                    </tr>
+                    <tr>
+                
+                        <td data-i18n="table.0.table2.row3.0">${data.table2.row3[0]}</td>
+                        <td data-i18n="table.0.table2.row3.1">${data.table2.row3[1]}</td>
+                        <tr>
+                        <td class="empty"></td>
+                        <td class="empty"></td>
+                      </tr>
+                      <tr>
+                        <td class="empty"></td>
+                        <td class="empty"></td>
+                      </tr>
+                      <tr>
+                        <td class="empty"></td>
+                        <td class="empty"></td>
+                      </tr>
+                      <tr>
+                        <td class="empty"></td>
+                        <td class="empty"></td>
+                      </tr>
+            
+            
+                    `;
+
+  //---Feestdagen---
+  table6.innerHTML = `
+                        
+                <th data-i18n="tableDateSpan.3" colspan="3">Vakanties</th>
+            
+                    <tr>
+                                <td data-i18n="table.0.table6.row1.0">${data.table6.row1[0]}</td>
+                                <td data-i18n="table.0.table6.row1.1">${data.table6.row1[1]}</td>
+                                <td data-i18n="table.0.table6.row1.2">${data.table6.row1[2]}</td>
+                                
+                            </tr>
+                            <tr>
+                       
+                        
+                                <td data-i18n="table.0.table6.row2.0">${data.table6.row2[0]}</td>
+                                <td data-i18n="table.0.table6.row2.1">${data.table6.row2[1]}</td>
+                                <td data-i18n="table.0.table6.row2.2">${data.table6.row2[2]}</td>
+                                
+            
+            
+                            </tr>
+                            <tr>
+                                <td data-i18n="table.0.table6.row3.0">${data.table6.row3[0]}</td>
+                                <td data-i18n="table.0.table6.row3.1">${data.table6.row3[1]}</td>
+                                <td data-i18n="table.0.table6.row3.2">${data.table6.row3[2]}</td>
+                                
+                            </tr>
+                            <tr>
+            
+                                <td data-i18n="table.0.table6.row4.0">${data.table6.row4[0]}</td>
+                                <td data-i18n="table.0.table6.row4.1">${data.table6.row4[1]}</td>
+                                <td data-i18n="table.0.table6.row4.2">${data.table6.row4[2]}</td>
+                                
+                            </tr>
+                            <tr>
+                                <td data-i18n="table.0.table6.row5.0">${data.table6.row5[0]}</td>
+                                <td data-i18n="table.0.table6.row5.1">${data.table6.row5[1]}</td>
+                                <td data-i18n="table.0.table6.row5.2">${data.table6.row5[2]}</td>
+                                
+                            </tr>
+                            <tr>
+                                <td data-i18n="table.0.table6.row6.0">${data.table6.row6[0]}</td>
+                                <td data-i18n="table.0.table6.row6.1">${data.table6.row6[1]}</td>
+                                <td data-i18n="table.0.table6.row6.2">${data.table6.row6[2]}</td>
+                                
+                            </tr>
+                            <tr>
+                                <td data-i18n="table.0.table6.row7.0">${data.table6.row7[0]}</td>
+                                <td data-i18n="table.0.table6.row7.1">${data.table6.row7[1]}</td>
+                                <td data-i18n="table.0.table6.row7.2">${data.table6.row7[2]}</td>
+                                
+                            </tr>
+                            <tr>
+                                <td data-i18n="table.0.table6.row8.0">${data.table6.row8[0]}</td>
+                                <td data-i18n="table.0.table6.row8.1">${data.table6.row8[1]}</td>
+                                <td data-i18n="table.0.table6.row8.2">${data.table6.row8[2]}</td>
+                                
+                            </tr>
+                            <tr>
+                                <td data-i18n="table.0.table6.row9.0">${data.table6.row9[0]}</td>
+                                <td data-i18n="table.0.table6.row9.1">${data.table6.row9[1]}</td>
+                                <td data-i18n="table.0.table6.row9.2">${data.table6.row9[2]}</td>
+                                
+                            </tr>
+                            <tr>
+                                <td data-i18n="table.0.table6.row10.0">${data.table6.row10[0]}</td>
+                                <td data-i18n="table.0.table6.row10.1">${data.table6.row10[1]}</td>
+                                <td data-i18n="table.0.table6.row10.2">${data.table6.row10[2]}</td>
+                                
+                            </tr>`;
 }
 
 // fetchOpeningsuren();
